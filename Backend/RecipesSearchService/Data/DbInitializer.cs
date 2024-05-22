42d8e03509efb8634a6315a9c253a93ee627fe5e
Joris Plaščinskas <PlascinskasJoris@gmail.com>
2024-05-22 22:16:37 +0300

    feat: add search service without link to auctions service

---
+++ Backend/RecipesSearchService/Data/DbInitializer.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json;
+using MongoDB.Driver;
+using MongoDB.Entities;
+using RecipesSearchService.Models;
+
+namespace RecipesSearchService.Data;
+
+public static class DbInitializer
+{
+    public static async Task InitializeDb(WebApplication app)
+    {
+        Console.WriteLine("DbInitializer: initializing and indexing...");
+        await DB.InitAsync("RecipesSearchDatabase", MongoClientSettings.FromConnectionString(app.Configuration.GetConnectionString("DockerMongoDbConnectionString")));
+        await DB.Index<RecipeSearchItem>()
+            .Key(x => x.Title, KeyType.Text)
+            .Key(x => x.ShortDescription, KeyType.Text)
+            .Key(x => x.IngredientsList, KeyType.Text)
+            .Key(x => x.CookingSteps, KeyType.Text)
+            .CreateAsync();
+
+        var count = await DB.CountAsync<RecipeSearchItem>();
+
+        if (count != 0)
+        {
+            Console.WriteLine("DbInitializer: data present, no need to seed...");
+            return;
+        }
+        else
+        {
+            Console.WriteLine("DbInitializer: no data present, attempting to seed...");
+
+            var itemData = await File.ReadAllTextAsync("Data/recipes.json");
+            var options = new JsonSerializerOptions{PropertyNameCaseInsensitive=true};
+            var items = JsonSerializer.Deserialize<List<RecipeSearchItem>>(itemData, options);
+            await DB.SaveAsync(items);
+
+            Console.WriteLine("DbInitializer: added seed data...");
+        }
+    }
+}
