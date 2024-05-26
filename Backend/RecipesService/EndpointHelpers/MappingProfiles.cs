1cb06b04db792a94136199df1687a7eeaf1747c3
Joris Plaščinskas <PlascinskasJoris@gmail.com>
2024-05-26 11:50:35 +0300

    feat: add rating to review response

--- Backend/RecipesService/EndpointHelpers/MappingProfiles.cs
+++ Backend/RecipesService/EndpointHelpers/MappingProfiles.cs
@@ -9,7 +9,8 @@ public class MappingProfiles : Profile
 {
     public MappingProfiles()
     {
-        CreateMap<Recipe, RecipeResponseDto>();
+        CreateMap<Recipe, RecipeResponseDto>()
+            .ForMember(dest => dest.Rating, opt => opt.MapFrom(src => src.Reviews.Any() ? (float)src.Reviews.Average(r => r.StarRating) : 0));
         CreateMap<RecipeCreateDto, Recipe>();
         // RecipeUpdateDto for now will have to be done manually
 
